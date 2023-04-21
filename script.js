283456683b6bce70c19eec6c85413cb8637d849f
duncan20903 <52763377+duncan20903@users.noreply.github.com>
2023-04-21 23:33:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function classChanger(newClass){
+  document.body.setAttribute('class',newClass)
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
